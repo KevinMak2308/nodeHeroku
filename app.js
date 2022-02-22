1df583fe2ff678f40ed93429579454e2cecea747
KevinMak2308 <kevi4196@stud.kea.dk>
2022-02-22 19:16:15 +0100

    Scripts in package.json 2

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 app.use(expres.json());
 
 
+app.get("/", (req, res) => {
+    res.send("Hey hey");
+});
+
 const port = process.env.PORT || 8080
 app.listen(port, () => { 
     console.log("The server is running on port", 8080);
